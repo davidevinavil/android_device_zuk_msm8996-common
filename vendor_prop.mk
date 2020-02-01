6256acf6943b5198ade5329284a79c8a342dd0d4
ZeeLog <Danjel.Zammit@gmail.com>
2020-02-01 20:57:28 +0100

    msm8996-common: prefer HW codec over SW for thumbnail
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -121,6 +121,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
     vendor.vidc.enc.disable.pq=true \
     vidc.enc.dcvs.extra-buff-count=2
 

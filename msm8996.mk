b5d0756f2a569e7a83ad6b48c31333126588a969
dmd79 <davidevinavil@gmail.com>
2021-03-20 12:41:58 +0100

    msm8996-common: Import qti power HAL and rename it to android.hardware.power-service-zuk

--- msm8996.mk
+++ msm8996.mk
@@ -332,8 +332,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power-service-zuk \
+    vendor.qti.hardware.perf@1.0 \
+    vendor.qti.hardware.perf@2.0
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
